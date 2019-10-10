078b9fe282309458bca159278964aeee3373370b
Jordan Sosa <jososa@s.sfusd.edu>
2019-10-10 00:10:42 +0000

    made 'ay' go to back of word and first letter also go back

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 $( "document" ).ready(function() {
     $("#button").click(function(){
-    var word = $(".input").val();
-    $(".output").append("<p>"+word+"</p>");
+    var vowels = ["a","e","i","o","u"];
+    var input = $(".input").val();
+    var word = input.toLowerCase();
+    if (word[0] || "a" || "i" || "o" || "u"){
+        let wordsVowel = word.slice(0,1);
+        let restOfWord = word.slice(1,-1);
+        $(".output").append("<p>"+restOfWord+wordsVowel+"ay</p>");
+    }else{
+        $(".output").append("<p>"+word+"ay</p>");
+    }
     });
 });
+
